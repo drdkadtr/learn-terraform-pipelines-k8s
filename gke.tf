87f86ad4de23b6ca818b324e038fe17d6285251c
Admir Trakic <90199729+drdkadtr@users.noreply.github.com>
2021-12-30 07:57:57 +0100

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -11,6 +11,16 @@ resource "google_container_cluster" "engineering" {
   initial_node_count       = 1
   
   ip_allocation_policy {}
+  
+  lifecycle {
+    ignore_changes = [node_pool, initial_node_count, resource_labels["asmv"], resource_labels["mesh_id"]]
+  }
+
+  timeouts {
+    create = "45m"
+    update = "45m"
+    delete = "45m"
+  } 
 }
 
 resource "google_container_node_pool" "engineering_preemptible_nodes" {
@@ -32,6 +42,16 @@ resource "google_container_node_pool" "engineering_preemptible_nodes" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
+  }
+ 
+   lifecycle {
+    ignore_changes = [initial_node_count]
+  }
+
+  timeouts {
+    create = "45m"
+    update = "45m"
+    delete = "45m"
   }
 }
 
