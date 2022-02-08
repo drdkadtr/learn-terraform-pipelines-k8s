6020d5ff2c6028ed86ac4139aad34c871593134a
Admir Trakic <adtr@dr.dk>
2022-02-08 15:21:26 +0100

    Add helm provider

--- providers.tf
+++ providers.tf
@@ -9,3 +9,11 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(google_container_cluster.current.master_auth[0].cluster_ca_certificate, )
   token                  = data.google_client_config.default.access_token
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.current.endpoint}"
+    cluster_ca_certificate = base64decode(google_container_cluster.current.master_auth[0].cluster_ca_certificate, )
+    token                  = data.google_client_config.default.access_token
+  }
+}
