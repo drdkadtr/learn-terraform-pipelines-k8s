142137d8030cebe259b1624e9cd281a75d495174
Admir Trakic <adtr@dr.dk>
2022-02-09 09:37:01 +0100

    Update

--- providers.tf
+++ providers.tf
@@ -2,19 +2,3 @@ provider "google" {
   project = var.google_project
   region  = var.region
 }
-
-provider "kubernetes" {
-  alias                  = "gke"
-  host                   = "https://${google_container_cluster.current.endpoint}"
-  cluster_ca_certificate = base64decode(google_container_cluster.current.master_auth[0].cluster_ca_certificate, )
-  token                  = data.google_client_config.default.access_token
-}
-
-provider "helm" {
-  alias = "gke"
-  kubernetes {
-    host                   = "https://${google_container_cluster.current.endpoint}"
-    cluster_ca_certificate = base64decode(google_container_cluster.current.master_auth[0].cluster_ca_certificate, )
-    token                  = data.google_client_config.default.access_token
-  }
-}
