d763657bbdca6e481ffab84864052141cb274d48
Admir Trakic <adtr@dr.dk>
2022-02-09 11:54:19 +0100

    Bump providers

--- terraform.tf
+++ terraform.tf
@@ -4,15 +4,7 @@ terraform {
     # https://registry.terraform.io/providers/hashicorp/google/latest
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.0.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.7.1"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.4.1"
+      version = ">= 4.10.0"
     }
   }
   required_version = ">= 1.0.0"
